270c0888c03fe506d5e78d21023edab38c96085a
Alfredo <alfredrobecerra@gmail.com>
2023-03-22 21:34:11 -0600

    ya guarda masomens

--- main.js
+++ main.js
@@ -17,67 +17,100 @@
           display.classList.remove("siDisplay");
           display.classList.add("noDisplay");
         })
+
+        let nuevosAcertijos = [];
+        let todosAcertijos = [...acertijos];
+        if(localStorage.getItem("nuevosAcertijos") != null){
+          nuevosAcertijos = JSON.parse(localStorage.getItem("nuevosAcertijos"))
+        }
         display.addEventListener("submit", (event) => {
           event.preventDefault();
   
-  
-          const acertijoForm = document.querySelector("#acertijo").value;
-          const respuestaForm = document.querySelector("#respuesta").value;
-          const dificultadForm = document.querySelector("#dificultad").value;
-          const linkForm = document.querySelector("#link").value;
-          nuevosAcertijos.push({acertijoForm, respuestaForm, dificultadForm, linkForm})
-          console.log(nuevosAcertijos)
-  
-        })
+          const acertijoInput = document.getElementById("acertijo");
+          const dificultadInput = document.getElementById("dificultad");
+          const respuestaInput = document.getElementById("respuesta");
+          const imagenInput = document.getElementById("imagen");
+          const nuevoAcertijo = {
+            acertijo: acertijoInput.value,
+            dificultad: dificultadInput.value,
+            respuesta: respuestaInput.value.toUpperCase(),
+            link: imagenInput.value
+          };
+        
+          nuevosAcertijos.push(nuevoAcertijo);
+          localStorage.setItem("nuevosAcertijos", JSON.stringify(nuevosAcertijos));
+          console.log(nuevosAcertijos);
+          form.reset();
+
+          display.classList.remove("siDisplay");
+          display.classList.add("noDisplay");
+        });
+
+
 
     let usarPredeterminados = true;
     const preUsar = document.getElementById("preUsar")
-    preUsar.addEventListener("click", () =>{
-        if(preUsar.classList.contains("Usar")){
-            preUsar.classList.remove("Usar");
-            preUsar.classList.add("noUsar");
-            preUsar.textContent = "No Usar Acertijos Por Defecto"
-            usarPredeterminados = false
-            console.log(usarPredeterminados)
-        }else{
-            preUsar.classList.remove("noUsar");
-            preUsar.classList.add("Usar");
-            preUsar.textContent = "Usar Acertijos Por Defecto"
-            usarPredeterminados = true
-            console.log(usarPredeterminados)
-        }
-    })
+    preUsar.textContent = "se complico y esta pendiente"
+  //  preUsar.addEventListener("click", () =>{
+  //      if(preUsar.classList.contains("Usar")){
+  //          preUsar.classList.remove("Usar");
+  //          preUsar.classList.add("noUsar");
+  //          preUsar.textContent = "No Usar Acertijos Por Defecto"
+  //          usarPredeterminados = false
+  //          console.log(usarPredeterminados)
+  //      }else{
+  //          preUsar.classList.remove("noUsar");
+  //          preUsar.classList.add("Usar");
+  //          preUsar.textContent = "Usar Acertijos Por Defecto"
+  //          usarPredeterminados = true
+  //          console.log(usarPredeterminados)
+  //      }
+  //  })        
+    
+ 
+   
 
-    const acertijosConNumero = nuevosAcertijos.map((acertijo, index) => {
-        return {
-          ...acertijo,
-          numero: index + 1
-        };
-      });
-      console.log(acertijosConNumero)
 
       //iniciarJuego
       function iniciarJuego() {
         preUsar.classList.add("jugando")
-        const nuevosAcertijos = []
-    if (usarPredeterminados = true) {
-      nuevosAcertijos.push(...acertijos);
-    } else{
-      nuevosAcertijos = []
-    }
+        todosAcertijos = [...acertijos, ...nuevosAcertijos];
+    const acertijosConNumero = todosAcertijos.map((acertijo, index) => {
+      return {
+        ...acertijo,
+        numero: index + 1
+      };
+
+    });
         let cualsera = Math.floor(Math.random() * acertijosConNumero.length) + 1;
-        console.log("te toco el acertijo numero " + cualsera);
+        if (cualsera > 3) {
+          console.log("Te tocó tu acertijo número " + (cualsera - 3));
+        }else{
+          console.log("te toco el acertijo predeterminado numero " + cualsera);
+        }
+  
         const elementoAcertijo = document.querySelector("#mostrar");
         elementoAcertijo.classList.remove("textoAcertijo");
         elementoAcertijo.textContent = "";
+
+        const tuRespuesta = document.querySelector("#tuRespuesta")
+        tuRespuesta.classList.remove("entonces")
+        tuRespuesta.classList.add("noEntonces")
       
         setTimeout(() => {
           const acertijoMostrar = acertijosConNumero.find(
             (acertijo) => acertijo.numero === cualsera
           );
+          console.log(acertijoMostrar)
           elementoAcertijo.classList.add("textoAcertijo");
           elementoAcertijo.textContent = acertijoMostrar.acertijo;
+          tuRespuesta.classList.remove("noEntonces")
+          tuRespuesta.classList.add("entonces")
         }, 200);
+
+        if(tuRespuesta.value.toUpperCase == acertijoMostrar.respuesta){
+
+        } 
       }
       
 
@@ -86,10 +119,8 @@
 
 
 
-const dificultad = parseInt(prompt("Elija la dificultad maxima que desea del 1 al 3"))
 const filtrar = acertijos.filter(acertijo => acertijo.dificultad <= dificultad)
-console.log("Estos son los acertijos que coincidian con la dificultad que escogiste")
-console.log(filtrar)
+
 
 let adivino = false;
 let cualaacertijo =0;
@@ -104,54 +135,5 @@ function resultado(){
        alert("Para ver detalles como la respuesta de los acertijoz y el numero de acertijo que te toco ve a la consola y para volver a jugar recarga la pagina")
 }
 
-if (jugar == "si" && adivino == false){   
- 
-   
 
-let respuestax = ""
-            switch (true) {
-                case cualsera >= 66 && cualsera <= 99:
-                    alert(acertijos[0].acertijo + " la dificultad es de " + acertijos[0].dificultad + "/3")
-                    cualaacertijo = 0
-                    while (respuestax.toUpperCase() != acertijos[0].respuesta){
-                        respuestax = prompt("su respuesta solo escriba que es ejemplo (manzana)").toUpperCase();
-                    }
-                    if (respuestax == acertijos[0].respuesta){
-                        adivino = true
-                    }
-                    resultado();
-                    break;
-                    
-                  case cualsera >= 33 && cualsera <= 65:
-                    alert(acertijos[1].acertijo + " la dificultad es de " + acertijos[1].dificultad + "/3");
-                    cualaacertijo = 1
-                    while (respuestax.toUpperCase() != acertijos[1].respuesta){
-                        respuestax = prompt("su respuesta solo escriba que es ejemplo (manzana)").toUpperCase();
-                    }
-                    if (respuestax == acertijos[1].respuesta){
-                        adivino = true
-                        }
-                        resultado();
-                    break;
-
-                  case cualsera >= 0 && cualsera <= 32:
-                    alert(acertijos[2].acertijo + " la dificultad es de " + acertijos[2].dificultad + "/3");
-                    cualaacertijo = 2
-                    while (respuestax.toUpperCase() != acertijos[2].respuesta){
-                        respuestax = prompt("su respuesta solo escriba que es ejemplo (manzana)").toUpperCase();
-                    }
-                    if (respuestax == acertijos[2].respuesta){
-                        adivino = true
-                        }
-                        resultado();
-                    break;
-                }
-};
-
-if(!adivino){
-alert("Vuelve a intentar suerte la proxima")
-console.log("No adivinaste")
-}else{
-    console.log("adivinaste")
-}
 
