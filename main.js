1f96abd0a3bef5bfcf5aa1a14d35d3155aa4f861
Alfredo <alfredrobecerra@gmail.com>
2023-04-04 22:07:18 -0600

    3.8

--- main.js
+++ main.js
@@ -3,13 +3,13 @@
     const acertijos = [
     { acertijo: "Tengo agujas pero no sé coser, tengo números pero no sé leer, ¿Sabes quién soy?", dificultad: 2, respuesta: "RELOJ", link: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fes%2Fsearch%2Freloj-analogico&psig=AOvVaw0WzGdkCQz3p9fXYKgV34Cj&ust=1679348281527000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCaxID66P0CFQAAAAAdAAAAABAT"},
     { acertijo: "No muerdo ni ladro, pero tengo dientes y la casa guardo. ¿Sabes quién soy?", dificultad: 3, respuesta: "LLAVE", link:"https://www.educima.com/dibujo-para-colorear-llave-dm22467.jpg"},
-    { acertijo: "Soy como un caballo pero no me montan y no tengo color ¿Sabes quién soy?", dificultad: 1, respuesta: "CEBRA", linl:"https://i.pinimg.com/originals/07/1c/81/071c811138f4a2dbebb8a817e3ac447c.png"}
+    { acertijo: "Soy como un caballo pero no me montan y no tengo color ¿Sabes quién soy?", dificultad: 1, respuesta: "CEBRA", link:"https://i.pinimg.com/originals/07/1c/81/071c811138f4a2dbebb8a817e3ac447c.png"}
         ]
 
         const btnAgregar = document.getElementById('agregar');   
         const btnCerrar = document.getElementById('cerrar');
         const display = document.getElementById("acertijo-form");
-        btnAgregar.addEventListener("click", () =>{
+        btnAgregar.addEventListener("click", function noDspl(){
           display.classList.remove("noDisplay");
           display.classList.add("siDisplay");
         })
@@ -25,16 +25,17 @@
         }
         display.addEventListener("submit", (event) => {
           event.preventDefault();
-  
+          display.classList.remove("siDisplay");
+          display.classList.add("noDisplay");
           const acertijoInput = document.getElementById("acertijo");
           const dificultadInput = document.getElementById("dificultad");
           const respuestaInput = document.getElementById("respuesta");
           const imagenInput = document.getElementById("imagen");
           const nuevoAcertijo = {
             acertijo: acertijoInput.value,
-            dificultad: dificultadInput.value,
+            dificultad: Number(dificultadInput.value),
             respuesta: respuestaInput.value.toUpperCase(),
-            link: imagenInput.value
+            link: imagenInput.value,
           };
         
           nuevosAcertijos.push(nuevoAcertijo);
@@ -66,53 +67,103 @@
   //          console.log(usarPredeterminados)
   //      }
   //  })        
-    
- 
-   
+  let compara = '';
+  const imagen = document.querySelector("#imagen");
+  const tuRespuesta = document.querySelector("#tuRespuesta");
+  tuRespuesta.addEventListener("change", (event) => {
+    compara = event.target.value.toUpperCase();
+  });
 
-
-      //iniciarJuego
-      function iniciarJuego() {
-        preUsar.classList.add("jugando")
-        todosAcertijos = [...acertijos, ...nuevosAcertijos];
+  //iniciarJuego
+  function iniciarJuego() {
+    preUsar.classList.add("jugando")
+    todosAcertijos = [...acertijos, ...nuevosAcertijos];
     const acertijosConNumero = todosAcertijos.map((acertijo, index) => {
       return {
         ...acertijo,
         numero: index + 1
       };
-
     });
-        let cualsera = Math.floor(Math.random() * acertijosConNumero.length) + 1;
-        if (cualsera > 3) {
-          console.log("Te tocó tu acertijo número " + (cualsera - 3));
-        }else{
-          console.log("te toco el acertijo predeterminado numero " + cualsera);
-        }
+    let cualsera = Math.floor(Math.random() * acertijosConNumero.length) + 1;
+    if (cualsera > 3) {
+      console.log("Te tocó tu acertijo número " + (cualsera - 3));
+    } else {
+      console.log("te toco el acertijo predeterminado numero " + cualsera);
+    }
   
-        const elementoAcertijo = document.querySelector("#mostrar");
-        elementoAcertijo.classList.remove("textoAcertijo");
-        elementoAcertijo.textContent = "";
-
-        const tuRespuesta = document.querySelector("#tuRespuesta")
-        tuRespuesta.classList.remove("entonces")
-        tuRespuesta.classList.add("noEntonces")
-      
-        setTimeout(() => {
-          const acertijoMostrar = acertijosConNumero.find(
-            (acertijo) => acertijo.numero === cualsera
-          );
-          console.log(acertijoMostrar)
-          elementoAcertijo.classList.add("textoAcertijo");
-          elementoAcertijo.textContent = acertijoMostrar.acertijo;
-          tuRespuesta.classList.remove("noEntonces")
-          tuRespuesta.classList.add("entonces")
-        }, 200);
-
-        if(tuRespuesta.value.toUpperCase == acertijoMostrar.respuesta){
-
-        } 
+    const elementoAcertijo = document.querySelector("#mostrar");
+      function noMostrarAcertijo(){
+      elementoAcertijo.classList.remove("textoAcertijo");
+      elementoAcertijo.textContent = "";
+      tuRespuesta.classList.remove("entonces")
+      tuRespuesta.classList.add("noEntonces")
+      imagen.classList.remove("entonces");
+      imagen.classList.add("noEntonces");
       }
-      
+      function MostrarAcertijo(){
+        elementoAcertijo.classList.add("textoAcertijo");
+        elementoAcertijo.textContent = acertijoMostrar.acertijo;
+        tuRespuesta.classList.remove("noEntonces");
+        tuRespuesta.classList.add("entonces");
+        imagen.classList.remove("noEntonces");
+        imagen.classList.add("entonces");
+}
+      acertijoMostrar = acertijosConNumero.find(
+        (acertijo) => acertijo.numero === cualsera
+      );
+      console.log(acertijoMostrar);
+      elementoAcertijo.classList.add("textoAcertijo");
+      elementoAcertijo.textContent = acertijoMostrar.acertijo;
+      tuRespuesta.classList.remove("noEntonces");
+      tuRespuesta.classList.add("entonces");
+      imagen.classList.remove("noEntonces");
+      imagen.classList.add("entonces");
+
+
+      const intervalo = setInterval(() => {
+        if (compara === acertijoMostrar.respuesta.toUpperCase()) {
+          clearInterval(intervalo);
+          tuRespuesta.classList.remove("incorrecta");
+          tuRespuesta.classList.add("correcta");
+ 
+          
+          setTimeout(() => {
+            Swal.fire({
+              title: 'Has Adivinado',
+              text: 'Bien Hecho',
+              icon: 'success',
+              toast: true,
+              confirmButtonText: 'OK',
+            })
+            noMostrarAcertijo()
+            tuRespuesta.value = ""
+          }, 1000);
+        } else {
+          tuRespuesta.classList.add("incorrecta");
+          setTimeout(() => {
+            tuRespuesta.classList.remove("correcta");
+          }, 1000);
+        }
+      }, 100);
+      }
+
+      // Obtener la imagen de perro al azar
+function obtenerImagenAlAzar() {
+  // Hacer una solicitud HTTP a la API
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+      const imagenF = data.message;
+      const imagenElemento = document.getElementById("imagenPerro");
+      imagenElemento.src = imagenF;
+    });
+}
+
+// Agregar una función de clic al botón de cambiar imagen
+const botonCambiarImagen = document.getElementById("boton-cambiar-imagen");
+
+
+
 
 
         
@@ -120,20 +171,3 @@
 
 
 const filtrar = acertijos.filter(acertijo => acertijo.dificultad <= dificultad)
-
-
-let adivino = false;
-let cualaacertijo =0;
-
-
-function resultado(){
-    if (adivino ){
-        alert("Felicidades esa era la respuesta correcta" )
-       }else{
-           alert("Esa no era la respuesta correcta vuelve a intentar")
-       }
-       alert("Para ver detalles como la respuesta de los acertijoz y el numero de acertijo que te toco ve a la consola y para volver a jugar recarga la pagina")
-}
-
-
-
